1d676f1adb46351a3e16c6e9c5d307e523ee3bf4
Sefatrp <Sefatrp@gmail.com>
2015-09-02 16:38:41 +0300

    Game

---
+++ ZarOyunu/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace ZarOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pcbZarBir = new System.Windows.Forms.PictureBox();
+            this.pcbZarIki = new System.Windows.Forms.PictureBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.gbTercih = new System.Windows.Forms.GroupBox();
+            this.rbKucuk = new System.Windows.Forms.RadioButton();
+            this.rbBuyuk = new System.Windows.Forms.RadioButton();
+            this.gbBahis = new System.Windows.Forms.GroupBox();
+            this.rbOtuz = new System.Windows.Forms.RadioButton();
+            this.rbYirmi = new System.Windows.Forms.RadioButton();
+            this.rbOn = new System.Windows.Forms.RadioButton();
+            this.lblOyunAdeti = new System.Windows.Forms.Label();
+            this.lblBilgPara = new System.Windows.Forms.Label();
+            this.lblOyuncuPara = new System.Windows.Forms.Label();
+            this.lblKasa = new System.Windows.Forms.Label();
+            this.tmrOyunZamani = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbZarBir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbZarIki)).BeginInit();
+            this.gbTercih.SuspendLayout();
+            this.gbBahis.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pcbZarBir
+            // 
+            this.pcbZarBir.Location = new System.Drawing.Point(52, 119);
+            this.pcbZarBir.Name = "pcbZarBir";
+            this.pcbZarBir.Size = new System.Drawing.Size(136, 76);
+            this.pcbZarBir.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcbZarBir.TabIndex = 0;
+            this.pcbZarBir.TabStop = false;
+            // 
+            // pcbZarIki
+            // 
+            this.pcbZarIki.Location = new System.Drawing.Point(232, 119);
+            this.pcbZarIki.Name = "pcbZarIki";
+            this.pcbZarIki.Size = new System.Drawing.Size(130, 76);
+            this.pcbZarIki.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcbZarIki.TabIndex = 1;
+            this.pcbZarIki.TabStop = false;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(232, 238);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(106, 23);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Oyna";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // gbTercih
+            // 
+            this.gbTercih.Controls.Add(this.rbKucuk);
+            this.gbTercih.Controls.Add(this.rbBuyuk);
+            this.gbTercih.Location = new System.Drawing.Point(475, 86);
+            this.gbTercih.Name = "gbTercih";
+            this.gbTercih.Size = new System.Drawing.Size(131, 100);
+            this.gbTercih.TabIndex = 3;
+            this.gbTercih.TabStop = false;
+            this.gbTercih.Text = "Tercih";
+            // 
+            // rbKucuk
+            // 
+            this.rbKucuk.AutoSize = true;
+            this.rbKucuk.Location = new System.Drawing.Point(16, 58);
+            this.rbKucuk.Name = "rbKucuk";
+            this.rbKucuk.Size = new System.Drawing.Size(56, 17);
+            this.rbKucuk.TabIndex = 1;
+            this.rbKucuk.TabStop = true;
+            this.rbKucuk.Text = "Küçük";
+            this.rbKucuk.UseVisualStyleBackColor = true;
+            // 
+            // rbBuyuk
+            // 
+            this.rbBuyuk.AutoSize = true;
+            this.rbBuyuk.Location = new System.Drawing.Point(16, 20);
+            this.rbBuyuk.Name = "rbBuyuk";
+            this.rbBuyuk.Size = new System.Drawing.Size(55, 17);
+            this.rbBuyuk.TabIndex = 0;
+            this.rbBuyuk.TabStop = true;
+            this.rbBuyuk.Text = "Büyük";
+            this.rbBuyuk.UseVisualStyleBackColor = true;
+            // 
+            // gbBahis
+            // 
+            this.gbBahis.Controls.Add(this.rbOtuz);
+            this.gbBahis.Controls.Add(this.rbYirmi);
+            this.gbBahis.Controls.Add(this.rbOn);
+            this.gbBahis.Location = new System.Drawing.Point(475, 229);
+            this.gbBahis.Name = "gbBahis";
+            this.gbBahis.Size = new System.Drawing.Size(131, 100);
+            this.gbBahis.TabIndex = 4;
+            this.gbBahis.TabStop = false;
+            this.gbBahis.Text = "Bahis";
+            // 
+            // rbOtuz
+            // 
+            this.rbOtuz.AutoSize = true;
+            this.rbOtuz.Location = new System.Drawing.Point(23, 69);
+            this.rbOtuz.Name = "rbOtuz";
+            this.rbOtuz.Size = new System.Drawing.Size(37, 17);
+            this.rbOtuz.TabIndex = 2;
+            this.rbOtuz.TabStop = true;
+            this.rbOtuz.Text = "30";
+            this.rbOtuz.UseVisualStyleBackColor = true;
+            // 
+            // rbYirmi
+            // 
+            this.rbYirmi.AutoSize = true;
+            this.rbYirmi.Location = new System.Drawing.Point(23, 42);
+            this.rbYirmi.Name = "rbYirmi";
+            this.rbYirmi.Size = new System.Drawing.Size(37, 17);
+            this.rbYirmi.TabIndex = 1;
+            this.rbYirmi.TabStop = true;
+            this.rbYirmi.Text = "20";
+            this.rbYirmi.UseVisualStyleBackColor = true;
+            // 
+            // rbOn
+            // 
+            this.rbOn.AutoSize = true;
+            this.rbOn.Location = new System.Drawing.Point(23, 19);
+            this.rbOn.Name = "rbOn";
+            this.rbOn.Size = new System.Drawing.Size(37, 17);
+            this.rbOn.TabIndex = 0;
+            this.rbOn.TabStop = true;
+            this.rbOn.Text = "10";
+            this.rbOn.UseVisualStyleBackColor = true;
+            // 
+            // lblOyunAdeti
+            // 
+            this.lblOyunAdeti.AutoSize = true;
+            this.lblOyunAdeti.Location = new System.Drawing.Point(475, 37);
+            this.lblOyunAdeti.Name = "lblOyunAdeti";
+            this.lblOyunAdeti.Size = new System.Drawing.Size(13, 13);
+            this.lblOyunAdeti.TabIndex = 5;
+            this.lblOyunAdeti.Text = "0";
+            // 
+            // lblBilgPara
+            // 
+            this.lblBilgPara.AutoSize = true;
+            this.lblBilgPara.Location = new System.Drawing.Point(99, 86);
+            this.lblBilgPara.Name = "lblBilgPara";
+            this.lblBilgPara.Size = new System.Drawing.Size(39, 13);
+            this.lblBilgPara.TabIndex = 6;
+            this.lblBilgPara.Text = "bilPara";
+            // 
+            // lblOyuncuPara
+            // 
+            this.lblOyuncuPara.AutoSize = true;
+            this.lblOyuncuPara.Location = new System.Drawing.Point(83, 49);
+            this.lblOyuncuPara.Name = "lblOyuncuPara";
+            this.lblOyuncuPara.Size = new System.Drawing.Size(66, 13);
+            this.lblOyuncuPara.TabIndex = 7;
+            this.lblOyuncuPara.Text = "OyuncuPara";
+            // 
+            // lblKasa
+            // 
+            this.lblKasa.AutoSize = true;
+            this.lblKasa.Location = new System.Drawing.Point(583, 37);
+            this.lblKasa.Name = "lblKasa";
+            this.lblKasa.Size = new System.Drawing.Size(13, 13);
+            this.lblKasa.TabIndex = 8;
+            this.lblKasa.Text = "0";
+            // 
+            // tmrOyunZamani
+            // 
+            this.tmrOyunZamani.Interval = 1500;
+            this.tmrOyunZamani.Tick += new System.EventHandler(this.tmrOyunZamani_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 387);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblKasa);
+            this.Controls.Add(this.lblOyuncuPara);
+            this.Controls.Add(this.lblBilgPara);
+            this.Controls.Add(this.lblOyunAdeti);
+            this.Controls.Add(this.gbBahis);
+            this.Controls.Add(this.gbTercih);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.pcbZarIki);
+            this.Controls.Add(this.pcbZarBir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbZarBir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbZarIki)).EndInit();
+            this.gbTercih.ResumeLayout(false);
+            this.gbTercih.PerformLayout();
+            this.gbBahis.ResumeLayout(false);
+            this.gbBahis.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbZarBir;
+        private System.Windows.Forms.PictureBox pcbZarIki;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.GroupBox gbTercih;
+        private System.Windows.Forms.RadioButton rbKucuk;
+        private System.Windows.Forms.RadioButton rbBuyuk;
+        private System.Windows.Forms.GroupBox gbBahis;
+        private System.Windows.Forms.RadioButton rbOtuz;
+        private System.Windows.Forms.RadioButton rbYirmi;
+        private System.Windows.Forms.RadioButton rbOn;
+        private System.Windows.Forms.Label lblOyunAdeti;
+        private System.Windows.Forms.Label lblBilgPara;
+        private System.Windows.Forms.Label lblOyuncuPara;
+        private System.Windows.Forms.Label lblKasa;
+        private System.Windows.Forms.Timer tmrOyunZamani;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ZarOyunu/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZarOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int oyun;
+        int bilgPara = 120;
+        int sayac;
+        int totalZarBir;
+        int zarToplam;
+        int totalZarIki;
+        int oyucuPara = 10;
+        int total;
+        int zarNumara;
+        Image[] ResimDizi = new Image[6];
+        Random rnd = new Random();
+
+        public void KucukKontrol() 
+        {
+            if (totalZarBir > totalZarIki)
+            {
+                if (oyun == 5)
+                {
+                    lblKasa.Text = (total / 2).ToString();
+                    total = total / 2;
+                }
+                bilgPara += total;               
+                lblBilgPara.Text = bilgPara.ToString();
+                lblOyuncuPara.Text = oyucuPara.ToString();
+            }
+            else if (totalZarBir < totalZarIki)
+            {
+                if (oyun == 5)
+                {
+                    lblKasa.Text = (total / 2).ToString();
+                    total = total / 2;
+                }
+                oyucuPara += total;
+                lblBilgPara.Text = bilgPara.ToString();
+                lblOyuncuPara.Text = oyucuPara.ToString();
+            }
+            else if (totalZarBir == totalZarIki)
+            {
+                if (oyun == 5)
+                {
+                    lblKasa.Text = (total / 2).ToString();
+                    total = total / 2;
+                }
+                oyucuPara += total / 2;
+                bilgPara += total / 2;
+                lblBilgPara.Text = bilgPara.ToString();
+                lblOyuncuPara.Text = oyucuPara.ToString();
+            }
+        }
+        public void BuyukKontrol() 
+        {
+            if (totalZarBir > totalZarIki)
+            {
+                if (oyun == 5)
+                {
+                    lblKasa.Text = (total / 2).ToString();
+                    total = total / 2;
+                }
+                oyucuPara += total;
+                lblBilgPara.Text = bilgPara.ToString();
+                lblOyuncuPara.Text = oyucuPara.ToString();
+            }
+            else if (totalZarBir < totalZarIki)
+            {
+                if (oyun == 5)
+                {
+                    lblKasa.Text = (total / 2).ToString();
+                    total = total / 2;
+                }
+                bilgPara += total;
+                lblBilgPara.Text = bilgPara.ToString();
+                lblOyuncuPara.Text = oyucuPara.ToString();
+            }
+            else if (totalZarBir == totalZarIki)
+            {
+                if (oyun == 5)
+                {
+                    lblKasa.Text = (total / 2).ToString();
+                    total = total / 2;
+                }
+                oyucuPara += total / 2;
+                bilgPara += total / 2;
+                lblBilgPara.Text = bilgPara.ToString();
+                lblOyuncuPara.Text = oyucuPara.ToString();
+            }
+        }
+        public int ResimAta() 
+        {
+            zarToplam = 0;
+            zarNumara = rnd.Next(0, 6);
+            zarToplam += (zarNumara+1);
+            pcbZarBir.Image = ResimDizi[zarNumara];
+            System.Threading.Thread.Sleep(50);
+            zarNumara = rnd.Next(0, 6);
+            zarToplam += (zarNumara+1);
+            pcbZarIki.Image = ResimDizi[zarNumara];
+            System.Threading.Thread.Sleep(50);
+            return zarToplam;           
+        }
+
+        public void Kontrol() 
+        {
+            if (oyucuPara == 0) 
+            
+            {
+                btnPlay.Enabled = false;
+                MessageBox.Show(" !! KAYBETTİN !!");
+                tmrOyunZamani.Stop();
+               
+                
+            }
+            if (bilgPara == 0) 
+            {
+                btnPlay.Enabled = false;
+                MessageBox.Show("!! KAZANDIN !!");
+                System.Threading.Thread.Sleep(1000);
+                Application.Exit();
+                tmrOyunZamani.Stop();
+            }
+
+            if (oyucuPara<30 || bilgPara<30)
+            {
+                rbOtuz.Enabled = false;
+            }
+            if  (oyucuPara < 20 || bilgPara < 20) 
+            {
+                rbYirmi.Enabled = false;
+            }
+            if (oyucuPara<10 || bilgPara<10)
+            {
+                rbOn.Enabled = false;
+                
+            }
+            if(oyucuPara>=10 && bilgPara>=10) 
+            {
+                rbOn.Enabled = true;
+            }
+            if (oyucuPara >= 20 && bilgPara >= 20)
+            {
+                rbYirmi.Enabled = true;
+            }
+            if (oyucuPara >= 30 && bilgPara >= 30)
+            {
+                rbOtuz.Enabled = true;
+
+            }
+
+        }
+         
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+           
+            
+            if (oyun == 5)
+                oyun = 0;
+            sayac++;
+            oyun++;
+            lblOyunAdeti.Text = sayac.ToString();         
+            totalZarBir = ResimAta();
+            label1.Text = totalZarBir.ToString();          
+            tmrOyunZamani.Start();
+            
+        }     
+        private void Form1_Load(object sender, EventArgs e)
+        {               
+            ResimDizi[0] = Properties.Resources.zar1;
+            ResimDizi[1] = Properties.Resources.zar2;
+            ResimDizi[2] = Properties.Resources.zar3;
+            ResimDizi[3] = Properties.Resources.zar4;
+            ResimDizi[4] = Properties.Resources.zar5;
+            ResimDizi[5] = Properties.Resources.zar6;           
+            lblBilgPara.Text = bilgPara.ToString();
+            lblOyuncuPara.Text = oyucuPara.ToString();       
+        }
+        private void tmrOyunZamani_Tick(object sender, EventArgs e)
+        {
+
+           
+
+            totalZarIki = ResimAta();
+            label2.Text = totalZarIki.ToString();
+            lblBilgPara.Text = bilgPara.ToString();
+            lblOyuncuPara.Text = oyucuPara.ToString();
+            if (rbOn.Checked)
+            {
+                bilgPara = bilgPara - 10;
+                oyucuPara = oyucuPara - 10;
+                total = 20;
+                if (rbBuyuk.Checked)
+                {
+                    BuyukKontrol();                 
+                }
+                if (rbKucuk.Checked)
+                {
+                    KucukKontrol();
+                }
+            }
+            if (rbYirmi.Checked)
+            {
+                bilgPara = bilgPara - 20;
+                oyucuPara = oyucuPara - 20;
+                total = 40;
+                if (rbBuyuk.Checked)
+                {
+                    BuyukKontrol();
+                }
+                if (rbKucuk.Checked)
+                {
+                    KucukKontrol();
+                }
+            }
+            if (rbOtuz.Checked)
+            {
+                bilgPara = bilgPara - 30;
+                oyucuPara = oyucuPara - 30;
+                total = 60;
+                if (rbBuyuk.Checked)
+                {
+                    BuyukKontrol();
+                }
+                if (rbKucuk.Checked)
+                {
+                    KucukKontrol();
+                }
+            }
+
+            Kontrol();
+
+            tmrOyunZamani.Stop();
+        }     
+    }
+}
